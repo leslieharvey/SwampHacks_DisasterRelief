f4f44b2c76978a6d98e8d9c07ce764853e118c64
luisaparicioufl <44217585+luisaparicioufl@users.noreply.github.com>
2020-02-02 09:47:07 -0500

    here

--- disasterrelief/lib/FirstScreen.dart
+++ disasterrelief/lib/FirstScreen.dart
@@ -1,83 +1,129 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/widgets.dart';
+import 'JobsListView.dart';
+
 
 void main() {
   runApp(FirstScreen());
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget{
+  @override
+  _FirstScreen createState() => _FirstScreen();
+
+}
+
+class _FirstScreen extends State<FirstScreen> {
 
   PageController _pageController;
-  int _page = 2;
-
-
+  WebViewController _controller;
   final icons = [Icons.list, Icons.map, Icons.settings, Icons.person];
   final titles = ['List', 'Map', 'Settings', 'Profile'];
-
+  int _page = 0;
 
   @override
   Widget build(BuildContext context) {
-    final title = 'List';
+
+    final title = 'Power Together';
+
+    @override
+    void initState() {
+      super.initState();
+      _pageController = new PageController();
+    }
+
+    @override
+    void dispose(){
+      super.dispose();
+      _pageController.dispose();
+    }
 
     return MaterialApp(
         title: title,
-        home: Scaffold(
-            drawer: Drawer(
-              child: ListView(
-                // Remove all padding form ListView
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  DrawerHeader(
-                    child: Text('Drawer Header'),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      image : DecorationImage(
-                          image: AssetImage("assets/logo.png"),
-                      fit: BoxFit.contain)
-                    )
-                    ),
-                  ListTile(
-                    title: Text('Home'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    title: Text('My Profile'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Map'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    title: Text('Settings'),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            ),
 
+        home: Scaffold(
             appBar: AppBar(
               title: Text(title),
             ),
-            body: PageView(
-              physics: NeverScrollableScrollPhysics(),
-              controller: _pageController,
-              children: <Widget>[
+            body: new PageView(
 
-              ],
+              children: <Widget>[
+                new Container(
+                    child : JobsListView()
+                ),
+                new Container(
+                  child: WebView(
+                    initialUrl: 'about:blank',
+                    onWebViewCreated: (WebViewController webViewController) {
+                    _controller = webViewController;
+                    _loadHtmlFromAssets('assets/frame.html',_controller);
+                    }
+                  )
+                ),
+                new Container(
 
                 )
+              ],
+                controller: _pageController,
+                onPageChanged: onPageChanged
+                ),
+          bottomNavigationBar:  new BottomNavigationBar(items: [
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.home),
+                title: new Text("Home")
+            ),
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.location_city),
+                title: new Text("Map")
+            ),
+            new BottomNavigationBarItem(
+                icon: new Icon(Icons.people),
+                title: new Text("Profile")
+            )
+          ],
+              onTap: navigationTapped,
+              currentIndex: _page
+          ),
+
         )
     );
   }
+  Future<void> _loadHtmlFromAssets(String filename, controller) async {
+    String fileText = await rootBundle.loadString(filename);
+    controller.loadUrl(Uri.dataFromString(fileText, mimeType: 'text/html', encoding: Encoding.getByName('utf-8')).toString());
+  }
+  void navigationTapped(int page){
+
+    _pageController.animateToPage(
+        page,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.bounceIn
+    );
+  }
+
+  void onPageChanged(int page){
+    setState((){
+      this._page = page;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = new PageController();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    _pageController.dispose();
+  }
 }
+
 
 abstract class ListItem {}
 
---
+++ disasterrelief/lib/JobsListView.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Job {
+  final String city;
+  final String state;
+  final List<String> tags;
+  final String text;
+  final int zipcode;
+
+  Job(this.city, this.state, this.tags, this.text,this.zipcode);
+
+  factory Job.fromJson(Map<String, dynamic> json) {
+    return Job(
+        json['city'],
+        json['state'],
+        json['tags'],
+        json['text'],
+        json['zip_code']
+    );
+  }
+}
+
+class JobsListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Job>>(
+      future: _fetchJobs(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Job> data = snapshot.data;
+          return _jobsListView(data);
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  Future<List<Job>> _fetchJobs() async {
+
+    final jobsListAPIUrl = 'https://disaster-relief-582e2.firebaseio.com/users.json';
+    final response = await http.get(jobsListAPIUrl);
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((job) => new Job.fromJson(job)).toList();
+    } else {
+      throw Exception('Failed to load jobs from API');
+    }
+  }
+
+  ListView _jobsListView(data) {
+    return ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return _tile(data[index].position, data[index].company, Icons.work);
+        });
+  }
+
+  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+    title: Text(title,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),
+    subtitle: Text(subtitle),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500],
+    ),
+  );
+}
